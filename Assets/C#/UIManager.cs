7efe7545c23e8b3ce64649b9554093bc2fe50394
2nd-JUIN <rmdmf259@semyung.ac.kr>
2025-04-09 21:27:39 +0900

    04.09. 업데이트

--- Assets/C#/UIManager.cs
+++ Assets/C#/UIManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
@@ -14,13 +15,127 @@ public class UIManager : MonoBehaviour
     public GameObject cleanlinessSurveyConfirmPanel; // 깔끔도 측정 확인 패널
     public GameObject surveyOnePagePanel; // 설문 1페이지 패널
     public GameObject surveyTwoPagePanel; // 설문 2페이지 패널
+    public GameObject signPanel; // 회원가입 및 로그인 패널
+    public TMP_InputField signInputID; // 회원가입 ID 항목
+    public TMP_InputField signInputPW; // 회원가입 PW 항목
+    public TMP_InputField signInputPWConfirm; // 회원가입 PW 확인 항목
+    public GameObject signSuccessPanel; // 회원가입 완료 패널
+    public TMP_Text registerErrorText; // Sign 에러 메시지 텍스트
+    public GameObject loginPanel; // 로그인 패널
+    public TMP_InputField loginInputID; // 로그인 ID 항목
+    public TMP_InputField loginInputPW; // 로그인 PW 항목
+    public TMP_Text LoginErrorText; // Login 에러 메세지 텍스트
+    public GameObject loginSuccessPanel; // 로그인 완료 패널
+
+
+
+
+
+    // 회원가입/로그인 버튼 클릭 시 실행
+    public void OpenSignPanel()
+    {
+        loginPanel.SetActive(false); // 로그인 패널 닫기
+        signPanel.SetActive(true); // 로그인/회원가입 패널 열기
+    }
+
+    // 로그인/회원가입 닫기 버튼 클릭 시 실행
+    public void CloseSignPanel()
+    {
+        signPanel.SetActive(false); // 로그인/회원가입 패널 닫기
+
+        signInputID.text = "";
+        signInputPW.text = "";
+        signInputPWConfirm.text = ""; // 닫기 시 도중에 입력한 데이터 초기화
+        registerErrorText.gameObject.SetActive(false); // 에러 메세지 비활성화
+    }
+    // 회원가입 버튼 클릭 시 실행                                                                      << 이 부분에서 회원가입에 대한 정보가 DB로 넘어가는 과정을 여기서 코딩할 듯 >>
+    public void OnRegisterButtonClicked()
+    {
+        string sgid = signInputID.text.Trim();
+        string sgpw = signInputPW.text.Trim();
+        string sgpwConfirm = signInputPWConfirm.text.Trim();
+
+        // 입력값이 비어있거나 비밀번호 불일치할 경우
+        if (string.IsNullOrEmpty(sgid) || string.IsNullOrEmpty(sgpw) || string.IsNullOrEmpty(sgpwConfirm) || sgpw != sgpwConfirm)
+        {
+            registerErrorText.gameObject.SetActive(true); // 에러 메세지 표시
+            return;
+        }
+        // 에러가 없을 경우
+        registerErrorText.gameObject.SetActive(false); // 에러 메세지 비활성화
+        signPanel.SetActive(false); // 기존 회원가입/로그인 패널 닫기
+        signInputID.text = "";
+        signInputPW.text = "";
+        signInputPWConfirm.text = ""; // 닫기 시 도중에 입력한 데이터 초기화
+        signSuccessPanel.SetActive(true); // 회원가입 완료 패널 열기
+
+        // 2초 후 자동 닫힘
+        StartCoroutine(CloseSignSuccessAfterDelay());
+    }
+    private IEnumerator CloseSignSuccessAfterDelay()
+    {
+        yield return new WaitForSeconds(2f); // 2초 딜레이
+        signSuccessPanel.SetActive(false);   // 패널 닫기
+    }
+
+
+
+
+    // 로그인 버튼 클릭 시 실행
+    public void OnLoginButtonClicked()
+    {
+        signInputID.text = "";
+        signInputPW.text = "";
+        signInputPWConfirm.text = ""; // 회원가입 정보 도중에 입력한 데이터 초기화
+        signPanel.SetActive(false);  // 기존 회원가입 패널 닫기
+        loginPanel.SetActive(true);  // 로그인 패널 열기
+    }
+
+    // 로그인 패널 닫기 버튼 클릭 시 실행
+    public void CloseLoginPanel()
+    {
+        loginInputID.text = ""; // ID 입력 초기화
+        loginInputPW.text = ""; // PW 입력 초기화
+        registerErrorText.gameObject.SetActive(false); // 회원가입 에러 메세지 비활성화
+        LoginErrorText.gameObject.SetActive(false); // 로그인 에러 메세지 비활성화
+        loginPanel.SetActive(false); // 로그인 패널 닫기
+    }
+
+    // 확인 버튼 클릭 시 실행                                                << 로그인 데이터 전송 ->  DB 회원가입된 데이터 비교해서 맞으면 로그인 완료, 맞지않으면 에러되는 과정으로 >>
+    public void OnLoginConfirmButtonClicked()
+    {
+        string lgnid = loginInputID.text.Trim();
+        string lgnpw = loginInputPW.text.Trim();
+        
+        // ID나 PW가 비어있으면 에러                                                        << 차 후 DB에서 회원가입 데이터와 맞지 않을때 에러 추가 >>
+        if (string.IsNullOrEmpty(lgnid) || string.IsNullOrEmpty(lgnpw))
+        {
+            LoginErrorText.gameObject.SetActive(true); // 에러 메세지 표시
+            return;
+        }
+        // 에러가 없을 경우
+        LoginErrorText.gameObject.SetActive(false); // 에러 메세지 비활성화
+        loginInputID.text = ""; // ID 입력 초기화
+        loginInputPW.text = ""; // PW 입력 초기화
+        loginPanel.SetActive(false); // 로그인 패널 닫기
+        loginSuccessPanel.SetActive(true); // 로그인 성공 패널 열기
+
+        // 2초 후 자동 닫힘
+        StartCoroutine(CloseLoginSuccessAfterDelay());
+    }
+    private IEnumerator CloseLoginSuccessAfterDelay()
+    {
+        yield return new WaitForSeconds(2f); // 2초 딜레이
+        loginSuccessPanel.SetActive(false);   // 패널 닫기
+    }
 
 
 
 
 
 
-    // 리더보드 버튼 클릭 시 실행
+
+    // 리더보드 버튼 클릭 시 실행                                                                          << 우진 여기는 내가 패널 디자인 다시 해야해서 아직 하지말아줘>>
     public void OpenLeaderBoard()
     {
         leaderBoardPanel.SetActive(true); // 리더보드 패널 열기
@@ -211,7 +326,7 @@ public class UIManager : MonoBehaviour
         surveyOnePagePanel.SetActive(true); // 설문 1페이지 열기
     }
 
-    // 설문 종료 후 '적용하기' 버튼 클릭 시 처리
+    // 설문 종료 후 '적용하기' 버튼 클릭 시 처리                                                       << 깔끔도 측정 데이터 DB로 전송하기 위한 코딩은 여기쯤 >>
     public void ApplySurveyResults()
     {
         surveyOnePagePanel.SetActive(false); // 설문 1페이지 닫기
